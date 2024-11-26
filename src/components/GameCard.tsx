4c79b75085e6e71b543af10e3a0b00bd7d2f8cb7
Lui Zappitelli <luizappitelli@gmail.com>
2024-11-25 18:46:29 -0600

    cleanup the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, HStack, Image, Text } from "@chakra-ui/react";
+import { Card, HStack, Heading, Image } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
@@ -13,13 +13,15 @@ const GameCard = ({ game }: Props) => {
   return (
     <Card.Root>
       <Image src={getCroppedImageUrl(game.background_image)} />
-      <Card.Body fontSize="2xl">{game.name}</Card.Body>
-      <HStack justifyContent="space-between">
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
-        <CriticScore score={game.metacritic} />
-      </HStack>
+      <Card.Body>
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
+      </Card.Body>
     </Card.Root>
   );
 };
