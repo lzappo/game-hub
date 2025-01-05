75e45f5be60b2f8442c73084efa26835121c24d6
Lui Zappitelli <luizappitelli@gmail.com>
2025-01-04 20:24:42 -0600

    fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,17 @@ interface Props {
 }
 
 const GameCardContainer = ({ children }: Props) => {
-  return <Box borderRadius={10}>{children}</Box>;
+  return (
+    <Box
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+      borderRadius={10}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default GameCardContainer;
